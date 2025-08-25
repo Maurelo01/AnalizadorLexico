5870bd732cbc030d29b7d8fada8d953c1f17805a
Maurelo01 <maurelo860@gmail.com>
2025-08-25 10:30:24 -0600

    Se añadieron las clases tokens

---
+++ AnalizadorLexico/src/main/java/com/mycompany/analizadorlexico/analisis/TipoToken.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.analizadorlexico.analisis;
+
+public enum TipoToken 
+{
+    RESERVADA,
+    IDENTIFICADOR,
+    NUMERO,
+    DECIMAL,
+    CADENA,
+    OPERADOR,
+    AGRUPACION,
+    PUNTUACION,
+    COMENTARIO,
+    ERROR
+}
